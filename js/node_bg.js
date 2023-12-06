33c81af86bf451c667d2ae7355df1c610e64517b
qimh0000 <100949740+qimh0000@users.noreply.github.com>
2023-12-06 16:22:10 +0800

    [Add] new videos & mute behind video

--- js/node_bg.js
+++ js/node_bg.js
@@ -40,7 +40,7 @@ function draw() {
     }
     for (let i = 0; i < numberOfNodes - 1; i++) {
         for (let c = i + 1; c < numberOfNodes; c++) {
-            fill(255,255,255,0);
+
             let d = sq(nodes[c].x - nodes[i].x) + sq(nodes[c].y - nodes[i].y);
             if (d < pow(90, 2)) {
                 stroke(255, map(d, 0, pow(100, 2), 100, 0));
@@ -48,10 +48,10 @@ function draw() {
                 nodes[c].radius=r+3;
                 
                 nodes[i].xv +=
-                    (0.00005 * (nodes[c].x - nodes[i].x) * nodes[c].radius) /
+                    (0.0005 * (nodes[c].x - nodes[i].x) * nodes[c].radius) /
                     nodes[i].radius;
                 nodes[i].yv +=
-                    (0.00005 * (nodes[c].y - nodes[i].y) * nodes[c].radius) /
+                    (0.0005 * (nodes[c].y - nodes[i].y) * nodes[c].radius) /
                     nodes[i].radius;
             }
         }
